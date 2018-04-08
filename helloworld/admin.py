6751a5990716ed850424c37b3c74829f453a4814
Victor Kim <hk1798@nyu.edu>
2018-04-08 17:56:09 -0400

    Linked login page to index

--- helloworld/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from helloworld.models import User
-# Register your models here.
-admin.site.register(User)
--- helloworld/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from helloworld import views
-
-urlpatterns = [
-    path('', views.users, name='users'),
-
-]
