5ffd2d5e11f257afd7f25980d2a555593cd83396
Victor Kim <hk1798@nyu.edu>
2018-04-22 01:15:20 -0400

    Now searchable by price, category, condition

--- items/search_indexes.py
+++ items/search_indexes.py
@@ -4,9 +4,15 @@ from items.models import Item
 
 class ItemIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    # Can also search by these fields...
+    price = indexes.CharField(model_attr='price')
+    condition = indexes.CharField(model_attr='condition')
+    category = indexes.CharField(model_attr='category')
+
     update_date = indexes.DateTimeField(model_attr='update_date')
     # NOT WORKING....
     content_auto = indexes.EdgeNgramField(model_attr='name')
+    
     def get_model(self):
         return Item
 
