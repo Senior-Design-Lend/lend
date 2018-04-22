b54346ede4fc7a1c1c4768173c5e8a0f3a4669fd
Victor Kim <hk1798@nyu.edu>
2018-04-22 15:57:03 -0400

    deleted chat's register page

--- chat/urls.py
+++ chat/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     path('api/messages', views.message_list, name='message-list'),
     path('api/users/<int:pk>', views.user_list, name='user-detail'),
     path('api/users', views.user_list, name='user-list'),
-    path('logout', logout, {'next_page': 'index'}, name='logout'),
-    path('register', views.register_view, name='register'),
 ]
