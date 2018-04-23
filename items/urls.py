d25f825cac250a895990035bad66d3955acba001
Jimmy Chang <chang.shengchi@nyu.edu>
2018-04-22 22:42:42 -0400

    only owner can delete and edit

--- items/urls.py
+++ items/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from items import views
+from django.contrib.auth.decorators import login_required
 
 app_name = 'items'
 
 urlpatterns = [
-    path('', views.listItemView.as_view(), name="list"),
-    path('<int:pk>/', views.detailItemView.as_view(), name="detail"),
-    path('addItem', views.createItemView.as_view(),name="create"),
-    path('update/<int:pk>/', views.updateItemView.as_view(), name="update"),
-    path('delete/<int:pk>/', views.deleteItemView.as_view(), name="delete"),
-    path('itemList', views.listItemView.as_view(), name='list'),
+    path('', login_required(views.listItemView.as_view()), name="list"),
+    path('<int:pk>/', login_required(views.detailItemView.as_view()), name="detail"),
+    path('addItem', login_required(views.createItemView.as_view()),name="create"),
+    path('update/<int:pk>/', login_required(views.updateItemView.as_view()), name="update"),
+    path('delete/<int:pk>/', login_required(views.deleteItemView.as_view()), name="delete"),
+    path('itemList', login_required(views.listItemView.as_view()), name='list'),
 
 ]
