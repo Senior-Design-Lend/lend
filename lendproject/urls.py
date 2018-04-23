b3c182d675e59ddd46c9a969d8f548b0ed0a9760
Victor Kim <hk1798@nyu.edu>
2018-04-23 15:33:41 -0400

    Kinda fixed navbar

--- lendproject/urls.py
+++ lendproject/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', include('login.urls')),
     path('logout/', views.user_logout, name='logout'),
-    path('', include('chat.urls'), name='chat'),
     path('home/', include('home.urls',namespace='home')),
     path('profile/', include('userprofile.urls'), name='profile'),
     path('items/', include('items.urls', namespace='items')),
