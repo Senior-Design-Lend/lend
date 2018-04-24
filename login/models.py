79600cd6dc63ef98f80a44d0b8104e280e280463
Jimmy Chang <chang.shengchi@nyu.edu>
2018-04-24 04:31:22 -0400

    fixed bug with transaction

--- login/models.py
+++ login/models.py
@@ -7,40 +7,24 @@ class UserProfileInfo(models.Model):
         ('None', 'None'),
         ('Appliances', 'Appliances'),
         ('Games', 'Games'),
-        ('Arts, Crafts, & Sewing', 'Arts, Crafts, & Sewing'),
-        ('Automotive Parts & Accessories', 'Automotive Parts & Accessories'),
         ('Baby & Baby Care', 'Baby & Baby Care'),
-        ('Beauty & Personal Care', 'Beauty & Personal Care'),
         ('Books', 'Books'),
         ('CDs & Vinyl', 'CDs & Vinyl'),
         ('Cell Phones & Accessories', 'Cell Phones & Accessories'),
         ('Clothing', 'Clothing'),
         ('Shoes', 'Shoes'),
-        ('Jewelry', 'Jewelry'),
-        ('Collectibles', 'Collectibles'),
-        ('Fine Art', 'Fine Art'),
         ('Computers', 'Computers'),
-        ('Cources', 'Cources'),
-        ('Credit & Payment Cards', 'Credit & Payment Cards'),
-        ('Digital Music', 'Digital Music'),
         ('Electronics', 'Electronics'),
         ('Garden & Outdoor', 'Garden & Outdoor'),
-        ('Gift Cards', 'Gift Cards'),
-        ('Grocery & Gourmet Food', 'Grocery & Gourmet Food'),
         ('Handmade', 'Handmade'),
         ('Health', 'Health'),
         ('Household', 'Household'),
-        ('Home & Business Services', 'Home & Business Services'),
         ('Home & Kitchen', 'Home & Kitchen'),
         ('Industrial & Scientific', 'Industrial & Scientific'),
         ('Luggage & Travel Gear', 'Luggage & Travel Gear'),
-        ('Luxury Beatuty', 'Luxury Beatuty'),
-        ('Magazine Subscriptions', 'Magazine Subscriptions'),
-        ('Movies & TV', 'Movies & TV'),
         ('Musical Instruments', 'Musical Instruments'),
         ('Office Products', 'Office Products'),
         ('Pet Supplies', 'Pet Supplies'),
-        ('Software', 'Software'),
         ('Sports & Outdoors', 'Sports & Outdoors'),
         ('Tools & Home Improvement', 'Tools & Home Improvement'),
         ('Toys & Games', 'Toys & Games'),
@@ -57,7 +41,6 @@ class UserProfileInfo(models.Model):
     interest1 = models.CharField(choices=CATEGORY_CHOICES, max_length=30, default='None')
     interest2 = models.CharField(choices=CATEGORY_CHOICES, max_length=30, default='None')
     interest3 = models.CharField(choices=CATEGORY_CHOICES, max_length=30, default='None')
-    ratings = models.DecimalField(max_digits=3, decimal_places=2, default='0')
     city = models.CharField(max_length=50, blank=True)
     state = models.CharField(choices=STATES, max_length=30, blank=True)
     profile_pic = models.ImageField(upload_to='profile_pic', blank=True)
--- resources/populateuser.py
+++ resources/populateuser.py
@@ -16,9 +16,7 @@ def populate(N=5):
         fake_last_name = fake_name[1]
         fake_email = fakegen.email()
         fake_password = fakegen.password(length=10,special_chars=True, digits=True, lower_case=True)
-        fake_rating = random.randint(100,500)/100
         fake_city = fakegen.city()
-        fake_state = fakegen.state()
         user = User.objects.get_or_create(first_name=fake_first_name, last_name= fake_last_name, email=fake_email, password=fake_password, ratings=fake_rating, city=fake_city, state=fake_state)[0]
     for item in range(N):
         fake_item_name = fakegen.street_name()
