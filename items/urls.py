7d910ff711fde9d3b8effbc4f60c217e6bd05a08
Jimmy Chang <chang.shengchi@nyu.edu>
2018-04-24 02:28:23 -0400

    fixed something

--- items/urls.py
+++ items/urls.py
@@ -5,11 +5,11 @@ from django.contrib.auth.decorators import login_required
 app_name = 'items'
 
 urlpatterns = [
-    path('', login_required(views.listItemView.as_view()), name="list"),
+    path('', views.listItemView.as_view(), name="list"),
     path('<int:pk>/', views.detailItemView.as_view(), name="detail"),
     path('addItem', login_required(views.createItemView.as_view()),name="create"),
     path('update/<int:pk>/', login_required(views.updateItemView.as_view()), name="update"),
     path('delete/<int:pk>/', login_required(views.deleteItemView.as_view()), name="delete"),
-    path('itemList', login_required(views.listItemView.as_view()), name='list'),
+    path('itemList', views.listItemView.as_view(), name='list'),
 
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -6,13 +6,24 @@ from django.views.generic import (View,TemplateView,
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from login.models import UserProfileInfo
+from django.contrib.auth.models import User
+
+from items.models import Item
 class profileView(TemplateView):
     template_name = "profile/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['pk'] = self.kwargs['pk']
+        ownerID = User.objects.get(id=self.kwargs['pk'])
+        context['items'] = Item.objects.filter(owner=ownerID)
+        context['picture'] = UserProfileInfo.objects.get(user=ownerID).profile_pic
+        context['first'] = User.objects.get(id=self.kwargs['pk']).first_name
+        context['last'] = User.objects.get(id=self.kwargs['pk']).last_name
+
+
         return context
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
+    # def dispatch(self, *args, **kwargs):
+    #     return super().dispatch(*args, **kwargs)
