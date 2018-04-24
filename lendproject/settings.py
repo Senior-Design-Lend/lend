aff31de9df7070f985811b6c644ea2382f6094ce
Farhan Hasan <fih207@nyu.edu>
2018-04-24 08:53:56 +0000

    fixing some profile stuff

--- lendproject/settings.py
+++ lendproject/settings.py
@@ -92,9 +92,9 @@ WSGI_APPLICATION = 'lendproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sample_db',
-        'USER': '',
-        'PASSWORD': '',
+        'NAME': 'lend',
+        'USER': 'ubuntu',
+        'PASSWORD': 'DBpassword',
         'HOST': 'localhost',
         'PORT': '',
     }
